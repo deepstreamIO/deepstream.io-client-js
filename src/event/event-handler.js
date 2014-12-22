53a3c80c330bab08c81d7e1070681a76d4beec4a
hoxton_one <info@golden-layout.com>
2014-12-22 15:10:00 +0000

    Added typing and tests for event-handler

--- src/event/event-handler.js
+++ src/event/event-handler.js
@@ -1,13 +1,35 @@
 var messageBuilder = require( '../message/message-builder' ),
+	messageParser = require( '../message/message-parser' ),
 	C = require( '../constants/constants' ),
 	EventEmitter = require( 'component-emitter' );
 
+/**
+ * This class handles incoming and outgoing messages in relation
+ * to deepstream events. It basically acts like an event-hub that's
+ * replicated across all connected clients.
+ *
+ * @param {Object} options    deepstream options
+ * @param {Connection} connection
+ *
+ * @public
+ * @constructor
+ */
 var EventHandler = function( options, connection ) {
 	this._options = options;
 	this._connection = connection;
 	this._emitter = new EventEmitter();
 };
 
+/**
+ * Subscribe to an event. This will receive both locally emitted events
+ * as well as events emitted by other connected clients.
+ *
+ * @param   {String}   eventName
+ * @param   {Function} callback
+ *
+ * @public
+ * @returns {void}
+ */
 EventHandler.prototype.subscribe = function( eventName, callback ) {
 	if( !this._emitter.hasListeners( eventName ) ) {
 		this._connection.sendMsg( C.TOPIC.EVENT, C.ACTIONS.SUBSCRIBE, [ eventName ] );
@@ -16,6 +38,17 @@ EventHandler.prototype.subscribe = function( eventName, callback ) {
 	this._emitter.on( eventName, callback );
 };
 
+/**
+ * Removes a callback for a specified event. If all callbacks
+ * for an event have been removed, the server will be notified
+ * that the client is unsubscribed as a listener
+ *
+ * @param   {String}   eventName
+ * @param   {Function} callback
+ *
+ * @public
+ * @returns {void}
+ */
 EventHandler.prototype.unsubscribe = function( eventName, callback ) {
 	this._emitter.off( eventName, callback );
 	
@@ -24,19 +57,36 @@ EventHandler.prototype.unsubscribe = function( eventName, callback ) {
 	}
 };
 
+/**
+ * Emits an event locally and sends a message to the server to 
+ * broadcast the event to the other connected clients
+ *
+ * @param   {String} name 
+ * @param   {Mixed} data will be serialized and deserialized to its original type.
+ *
+ * @public
+ * @returns {void}
+ */
 EventHandler.prototype.emit = function( name, data ) {
-	this._connection.sendMsg( C.TOPIC.EVENT, C.ACTIONS.EVENT, [ name, data ] );
+	this._connection.sendMsg( C.TOPIC.EVENT, C.ACTIONS.EVENT, [ name, messageBuilder.typed( data ) ] );
 	this._emitter.emit( name, data );
 };
 
+/**
+ * Handles incoming messages from the server
+ *
+ * @param   {Object} message parsed deepstream message
+ *
+ * @package privat
+ * @returns {void}
+ */
 EventHandler.prototype._$handle = function( message ) {
 	if( message.action === C.ACTIONS.EVENT ) {
 		if( message.data && message.data.length === 2 ) {
-			this._emitter.emit( message.data[ 0 ], message.data[ 1 ] );
+			this._emitter.emit( message.data[ 0 ], messageParser.convertTyped( message.data[ 1 ] ) );
 		} else {
 			this._emitter.emit( message.data[ 0 ] );
 		}
-		
 	}
 };
 
---
+++ test-unit/unit/event/event-handlerSpec.js
@@ -0,0 +1,65 @@
+/* global describe, expect, it, jasmine */
+var EventHandler = require( '../../../src/event/event-handler' ),
+	connectionMock = new (require( '../../mocks/message/connection-mock' ))(),
+	msg = require( '../../test-helper/test-helper' ).msg;
+	
+describe( 'event handler works', function(){
+	var eventHandler,
+		callback = jasmine.createSpy( 'eventCallback' );
+	
+	it( 'creates the eventHandler', function(){
+		eventHandler = new EventHandler( {}, connectionMock );
+		expect( eventHandler.emit ).toBeDefined();
+	});
+	
+	it( 'emits an event it has no listeners for', function(){
+		expect( connectionMock.lastSendMessage ).toBe( null );
+		eventHandler.emit( 'myEvent', 6 );
+		expect( connectionMock.lastSendMessage ).toBe( msg( 'EVENT|EVT|myEvent|N6+' ) );
+	});
+	
+	it( 'subscribes to an event', function() {
+	    eventHandler.subscribe( 'myEvent', callback );
+	    expect( connectionMock.lastSendMessage ).toBe( msg( 'EVENT|S|myEvent+' ) );
+	});
+	
+	it( 'notifies local listeners for local events', function() {
+		expect( callback ).not.toHaveBeenCalled();
+	    eventHandler.emit( 'myEvent', 8 );
+	    expect( callback ).toHaveBeenCalledWith( 8 );
+	});
+	
+	it( 'notifies local listeners for remote events', function() {
+		eventHandler._$handle({
+			topic: 'EVENT',
+			action: 'EVT',
+			data: [ 'myEvent', 'N23' ]
+		});
+		
+	    expect( callback ).toHaveBeenCalledWith( 23 );
+	});
+	
+	it( 'notifies local listeners for remote events without data', function() {
+		eventHandler._$handle({
+			topic: 'EVENT',
+			action: 'EVT',
+			data: [ 'myEvent' ]
+		});
+		
+	    expect( callback ).toHaveBeenCalledWith();
+	});
+	
+	it( 'removes local listeners', function() {
+		eventHandler.unsubscribe( 'myEvent', callback );
+		eventHandler.emit( 'myEvent', 11 );
+		expect( callback ).toHaveBeenCalledWith();
+	});
+	
+	it( 'doesn\'t do anything for unsolicited event messages', function() {
+		eventHandler._$handle({
+			topic: 'EVENT',
+			action: 'EVT',
+			data: [ 'myEvent' ]
+		});
+	});
+});
