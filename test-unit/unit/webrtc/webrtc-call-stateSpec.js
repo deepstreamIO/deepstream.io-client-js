504089ec816da433ae11530921badabbfd44ce87
Yasser Fadl <yasser.fadl@gmail.com>
2015-08-19 12:51:02 +0200

    Adding missing assertions for webrtc state spec

--- test-unit/unit/webrtc/webrtc-call-stateSpec.js
+++ test-unit/unit/webrtc/webrtc-call-stateSpec.js
@@ -5,8 +5,17 @@ var WebRtcCall = require( '../../../src/webrtc/webrtc-call.js' ),
 	webrtcMock = require( '../../mocks/webrtc/webrtc-mock' ),
 	options = { calleeAckTimeout: 5 };
 
+var declinedListener;
+var establishedListener;
+var stateChangeListener;
+var endedListener;
+
 function createCall( outgoing, mockConnection ) {
-	return new WebRtcCall({
+	declinedListener = jasmine.createSpy(),
+	stateChangeListener = jasmine.createSpy();
+	establishedListener = jasmine.createSpy();
+	endedListener = jasmine.createSpy();
+	var webrtcCall =  new WebRtcCall({
 			isOutgoing: outgoing,
 			connection: mockConnection, 
 			localId: 'localId', 
@@ -15,6 +24,15 @@ function createCall( outgoing, mockConnection ) {
 			offer: null,
 			metaData: { some: 'meta-data' }
 	});
+	webrtcCall.on( 'declined', declinedListener );
+	webrtcCall.on( 'stateChange', stateChangeListener );
+	webrtcCall.on( 'ended', endedListener );
+	webrtcCall.on( 'established', establishedListener );
+
+	expect( webrtcCall.isDeclined ).toBe( false );
+	expect( webrtcCall.isAccepted ).toBe( false );
+
+	return webrtcCall;
 }
 
 describe( 'WEBRTC call spec', function() {
@@ -34,15 +52,26 @@ describe( 'WEBRTC call spec', function() {
 		});
 
 		it( 'declining an incoming call changes its state to declined', function(){
-			expect( webrtcCall.isDeclined ).toBe( false );
-			expect( webrtcCall.isAccepted ).toBe( false );
 			webrtcCall.decline( 'some reason' );
+			
 			expect( webrtcCall.isDeclined ).toBe( true );
 			expect( webrtcCall.isAccepted ).toBe( false );
 			expect( mockConnection.lastSendMessage ).toBe( msg( 'W|CD|localId|remoteId|some reason+' ) );
 			expect( webrtcCall.state ).toBe( 'DECLINED' );
-			//TODO: Add declined event
-			//TODO: Add state changed event
+			expect( declinedListener ).toHaveBeenCalledWith( 'some reason' );
+			expect( stateChangeListener ).toHaveBeenCalledWith( 'DECLINED' );
+		});
+
+		it( 'can\'t accept declined calls', function(){
+			expect(function(){
+				webrtcCall.decline();
+			}).toThrow();
+		});
+
+		it( 'can\'t decline declined calls multiple times', function(){
+			expect(function(){
+				webrtcCall.decline();
+			}).toThrow();
 		});
 	});
 
@@ -57,28 +86,45 @@ describe( 'WEBRTC call spec', function() {
 
 		it( 'accepting an incoming call creates a peer connection and changes its state to accepted', function(){
 			expect( webrtcCall._$webRtcConnection ).toBe( null );
-			expect( webrtcCall.isDeclined ).toBe( false );
-			expect( webrtcCall.isAccepted ).toBe( false );
+			
 			webrtcCall.accept();
+			
 			expect( webrtcCall.isDeclined ).toBe( false );
 			expect( webrtcCall.isAccepted ).toBe( true );
 			expect( webrtcCall._$webRtcConnection._peerConnection.addStream.calls.length ).toBe( 0 );
 			expect( webrtcCall.state ).toBe( 'ACCEPTED' );
-			//TODO: Add state changed event
+			expect( stateChangeListener ).toHaveBeenCalledWith( 'ACCEPTED' );
+		});
+
+		it( 'can\'t accept accepted calls multiple times', function(){
+			expect(function(){
+				webrtcCall.accept();
+			}).toThrow();
+		});
+
+		it( 'can\'t decline accepted calls', function(){
+			expect(function(){
+				webrtcCall.decline();
+			}).toThrow();
 		});
 
 		it( 'receiving a remote stream changes the state to established', function(){
 			webrtcCall._$webRtcConnection._peerConnection.onaddstream({});
+			
 			expect( webrtcCall.state ).toBe( 'ESTABLISHED' );
-			//TODO: Add state changed event
+			expect( stateChangeListener ).toHaveBeenCalledWith( 'ESTABLISHED' );
+			expect( establishedListener ).toHaveBeenCalled();
 		});
 
 		it( 'receiving a end message closes the peer connection and sets the state to ended', function(){
 			expect( webrtcCall._$webRtcConnection._peerConnection.close ).not.toHaveBeenCalled();
+			
 			webrtcCall._$close();
+			
 			expect( webrtcCall.state ).toBe( 'ENDED' );
 			expect( webrtcCall._$webRtcConnection._peerConnection.close ).toHaveBeenCalled();
-			//TODO: Add state changed event
+			expect( stateChangeListener ).toHaveBeenCalledWith( 'ENDED' );
+			expect( endedListener ).toHaveBeenCalled();
 		});
 	});
 
@@ -92,10 +138,11 @@ describe( 'WEBRTC call spec', function() {
 		});
 
 		it( 'receives a decline message', function(){
-			expect( webrtcCall.isDeclined ).toBe( false );
-			expect( webrtcCall.isAccepted ).toBe( false );
 			webrtcCall._$declineReceived( 'a reason' );
+			
 			expect( webrtcCall.state ).toBe( 'DECLINED' );
+			expect( stateChangeListener ).toHaveBeenCalledWith( 'DECLINED' );
+			expect( declinedListener ).toHaveBeenCalled();
 			expect( webrtcCall.isDeclined ).toBe( true );
 			expect( webrtcCall.isAccepted ).toBe( false );
 		});
@@ -111,21 +158,24 @@ describe( 'WEBRTC call spec', function() {
 		});
 
 		it( 'receives a stream', function(){
-			expect( webrtcCall.isDeclined ).toBe( false );
-			expect( webrtcCall.isAccepted ).toBe( false );
-
 			webrtcCall._$webRtcConnection._peerConnection.onaddstream({});
+			
 			expect( webrtcCall.state ).toBe( 'ESTABLISHED' );
-
+			expect( stateChangeListener ).toHaveBeenCalledWith( 'ESTABLISHED' );
+			
 			expect( webrtcCall.isDeclined ).toBe( false );
 			expect( webrtcCall.isAccepted ).toBe( true );
 		});
 
 		it( 'ends the call', function(){
 			expect( webrtcCall._$webRtcConnection._peerConnection.close ).not.toHaveBeenCalled();
+			
 			webrtcCall.end();
+			
+			expect( mockConnection.lastSendMessage ).toBe( msg( 'W|CE|localId|remoteId|null+' ) );
 			expect( webrtcCall._$webRtcConnection._peerConnection.close ).toHaveBeenCalled();
 			expect( webrtcCall.state ).toBe( 'ENDED' );
+			expect( stateChangeListener ).toHaveBeenCalledWith( 'ENDED' );
 		});
 	});
 
