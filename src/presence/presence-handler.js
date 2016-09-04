a7d7581e206d00b1f11ce32a6e048bd832e1683c
Alex <alexbruceharley@gmail.com>
2016-09-04 18:43:14 +1200

    getting rid of class syntax

--- src/presence/presence-handler.js
+++ src/presence/presence-handler.js
@@ -1,44 +1,47 @@
 'use strict';
 
-var C = require( '../constants/constants' ),
+var EventEmitter = require( 'component-emitter' ),
+	C = require( '../constants/constants' ),
+	AckTimeoutRegistry = require( '../utils/ack-timeout-registry' ),
 	messageParser = require( '../message/message-parser' ),
 	messageBuilder = require( '../message/message-builder' );
 
-module.exports = class PresenceHandler {
-
-	constructor( options, connection, client ) {
+var PresenceHandler = function( options, connection, client ) {
 		this._options = options;
 		this._connection = connection;
 		this._client = client;
-		this._clientAddedCallback = null;
-		this._clientRemovedCallback = null;
-	}
+		this._emitter = new EventEmitter();
+		this._ackTimeoutRegistry = new AckTimeoutRegistry( client, C.TOPIC.PRESENCE, this._options.subscriptionTimeout );
+};
 
-	getCurrentClients( callback ) {
-		this._presentClientsCallback = callback;
-		var msg = messageBuilder.getMsg( C.TOPIC.PRESENCE, C.ACTIONS.QUERY );
-		this._connection.send( msg );
-	}
+PresenceHandler.prototype.getCurrentClients( callback ) {
+	this._emitter.on( C.ACTIONS.QUERY, callback );
+	this._presentClientsCallback = callback;
+	this._ackTimeoutRegistry.add( C.ACTIONS.QUERY );
+	this._connection.sendMsg( C.TOPIC.PRESENCE, C.ACTIONS.QUERY );
+};
 
-	onClientLogin( callback ) {
-		this._clientAddedCallback = callback;
+PresenceHandler.prototype.onClientLogin( callback ) {
+	this._emitter.on( C.ACTIONS.PRESENCE_JOIN, callback );
+	this._ackTimeoutRegistry.add( C.ACTIONS.PRESENCE_JOIN, C.ACTIONS.SUBSCRIBE );
+	this._connection.sendMsg( C.TOPIC.PRESENCE, C.ACTIONS.SUBSCRIBE, [ C.ACTIONS.PRESENCE_JOIN ] );
+};
 
-	}
+PresenceHandler.prototype.onClientLogout( callback ) {
+	this._emitter.on( C.ACTIONS.PRESENCE_LEAVE, callback );
+	this._ackTimeoutRegistry.add( C.ACTIONS.PRESENCE_LEAVE, C.ACTIONS.SUBSCRIBE );
+	this._connection.sendMsg( C.TOPIC.PRESENCE, C.ACTIONS.SUBSCRIBE, [ C.ACTIONS.PRESENCE_LEAVE ] );
+};
 
-	onClientLogout( callback ) {
-		this._clientRemovedCallback = callback;
+PresenceHandler.prototype._$handle( message ) {
+	if( message.action === C.ACTIONS.PRESENCE_JOIN ) {
+		this._emitter.emit( C.ACTIONS.PRESENCE_JOIN, message.data );
 	}
-
-	_$handle( message ) {
-		if( message.action === C.ACTIONS.PRESENCE_ADD && this._clientAddedCallback !== null ) {
-			this._clientAddedCallback( message.data );
-		}
-		else if( message.action === C.ACTIONS.PRESENCE_REMOVE && this._clientRemovedCallback !== null ) {
-			this._clientRemovedCallback( message.data );
-		}
-		else if( message.action === C.ACTIONS.QUERY ) {
-			this._presentClientsCallback( message.data );
-		}
+	else if( message.action === C.ACTIONS.PRESENCE_LEAVE ) {
+		this._emitter.emit( C.ACTIONS.PRESENCE_LEAVE, message.data );
 	}
-}
+	else if( message.action === C.ACTIONS.QUERY ) {
+		this._emitter.emit( C.ACTIONS.QUERY, message.data );
+	}
+};
 
