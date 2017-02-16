c75fc99af6baedda56858473abe9578c58c9785f
Robert Nagy <ronag89@gmail.com>
2017-02-16 08:13:11 +0100

    Don't destroy non-ready records.

--- src/record/record-handler.js
+++ src/record/record-handler.js
@@ -22,7 +22,7 @@ RecordHandler.prototype._prune = function () {
     let n = 0
     let k = prev.length
     while (n < k) {
-      if (prev[n].usages === 0) {
+      if (prev[n].usages === 0 && prev[n].isReady) {
         this._records.delete(prev[n].name)
         prev[n]._$destroy()
         prev[n] = prev[--k]
