5ce4ce8414208700831d644068a2c0ac530d16f1
Yasser Fadl <yasser.fadl@gmail.com>
2017-10-30 17:32:32 +0100

    test: ack timeout registry

---
+++ src/rpc/rpc-response.ts
@@ -0,0 +1,118 @@
+import { Services } from '../client'
+import { Options } from '../client-options'
+import { nextTick } from '../util/utils'
+import { TOPIC, RPC_ACTION, EVENT } from '../constants'
+
+/**
+ * This class represents a single remote procedure
+ * call made from the client to the server. It's main function
+ * is to encapsulate the logic around timeouts and to convert the
+ * incoming response data
+ */
+export class Rpc {
+    private services: Services
+    private options: Options
+    private name: string
+    private correlationId: string
+    private isAccepted: boolean
+    private isComplete: boolean
+    private autoAccept: boolean
+
+    constructor (name: string, correlationId: any, options: Options, services: Services) {
+        this.name = name
+        this.correlationId = correlationId
+        this.options = options
+        this.services = services
+        this.isAccepted = false
+        this.isComplete = false
+        this.autoAccept = true
+        nextTick(this.performAutoAck.bind(this))
+    }
+
+    /**
+     * Acknowledges the receipt of the request. This
+     * will happen implicitly unless the request callback
+     * explicitly sets autoAck to false
+     */
+    public accept () {
+      if (this.isAccepted === false) {
+        this.services.connection.sendMessage({
+          topic: TOPIC.RPC,
+          action: RPC_ACTION.ACCEPT,
+          name: this.name,
+          correlationId: this.correlationId
+        })
+        this.isAccepted = true
+      }
+    }
+
+    /**
+     * Reject the request. This might be necessary if the client
+     * is already processing a large number of requests. If deepstream
+     * receives a rejection message it will try to route the request to
+     * another provider - or return a NO_RPC_PROVIDER error if there are no
+     * providers left
+     */
+    public reject (): void {
+      this.autoAccept = false
+      this.isComplete = true
+      this.isAccepted = true
+      this.services.connection.sendMessage({
+        topic: TOPIC.RPC,
+        action: RPC_ACTION.REJECT,
+        name: this.name,
+        correlationId: this.correlationId
+      })
+    }
+
+    /**
+     * Notifies the server that an error has occured while trying to process the request.
+     * This will complete the rpc.
+     */
+    public error (error: any): void {
+      this.autoAccept = false
+      this.isComplete = true
+      this.isAccepted = true
+      this.services.connection.sendMessage({
+        topic: TOPIC.RPC,
+        action: RPC_ACTION.REQUEST_ERROR,
+        name: this.name,
+        correlationId: this.correlationId,
+        parsedData: error
+      })
+    }
+
+    /**
+     * Completes the request by sending the response data
+     * to the server. If data is an array or object it will
+     * automatically be serialised.
+     * If autoAck is disabled and the response is sent before
+     * the ack message the request will still be completed and the
+     * ack message ignored
+     */
+    public send (data: any): void {
+      if (this.isComplete === true) {
+        throw new Error(`Rpc ${this.name} already completed`)
+      }
+      this.accept()
+
+      this.services.connection.sendMessage({
+        topic: TOPIC.RPC,
+        action: RPC_ACTION.RESPONSE,
+        name: this.name,
+        correlationId: this.correlationId,
+        parsedData: data
+      })
+      this.isComplete = true
+    }
+
+    /**
+     * Callback for the autoAck timeout. Executes ack
+     * if autoAck is not disabled
+     */
+    private performAutoAck (): void {
+      if (this.autoAccept === true) {
+        this.accept()
+      }
+    }
+}
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,12 +1,12 @@
-import { TOPIC, EVENT_ACTION, EVENT } from '../constants'
+import { TOPIC, EVENT_ACTION, RECORD_ACTION, RPC_ACTION, EVENT } from '../constants'
 
 export class Logger {
-    public warn (message: Message, event?: EVENT, log?: string): void {
+    public warn (message: Message, event?: EVENT | EVENT_ACTION | RECORD_ACTION | RPC_ACTION, log?: string): void {
         // tslint:disable-next-line:no-console
         console.warn(message, event, log)
     }
 
-    public error (message: Message, event?: EVENT, log?: string): void {
+    public error (message: Message, event?: EVENT| EVENT_ACTION | RECORD_ACTION | RPC_ACTION, log?: string): void {
         // tslint:disable-next-line:no-console
         console.error(message, event, log)
     }
