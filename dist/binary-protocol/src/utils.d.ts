a303a4758ce7589543ff2651a4ecc1501c230fab
Murilo Frade <murilofrade@gmail.com>
2017-11-17 15:51:32 +0100

    chore: updating submodule

--- dist/binary-protocol/src/utils.d.ts
+++ dist/binary-protocol/src/utils.d.ts
@@ -3,3 +3,14 @@ export declare function isWriteAck(action: RA): boolean;
 export declare const ACTION_TO_WRITE_ACK: {
     [key: number]: RA;
 };
+/**
+ * Like reverseMap but the values will be cast using Number(k)
+ */
+export declare function reverseMapNumeric(map: {
+    [k: number]: number;
+}): {
+    [k: number]: number;
+};
+export declare const WRITE_ACK_TO_ACTION: {
+    [k: number]: number;
+};
