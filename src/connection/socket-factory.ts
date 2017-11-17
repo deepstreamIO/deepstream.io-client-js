84a62611acd57a55c13517c6adb62c978f79b577
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2017-11-17 17:07:08 +0000

    fix: added casting to buffer for browsers

--- src/connection/socket-factory.ts
+++ src/connection/socket-factory.ts
@@ -24,7 +24,7 @@ export const socketFactory = (url: string, options: any): Socket => {
     // tslint:disable-next-line:no-empty
     socket.onparsedmessage = () => {}
     socket.onmessage = (raw: {data: Buffer}) => {
-        const parseResults = parse(raw.data)
+        const parseResults = parse(BrowserWebsocket ? new Buffer(new Uint8Array(raw.data)) :raw.data)
         socket.onparsedmessages(parseResults)
     }
     socket.sendParsedMessage = (message: Message): void => {
