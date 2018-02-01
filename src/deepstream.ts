66a93fd8020d267be9aa30f5941f6aec844cd435
Thomas-P <tpx.dev@gmail.com>
2018-02-01 09:56:19 +0100

    Provide a better interface for creation

--- src/deepstream.ts
+++ src/deepstream.ts
@@ -3,12 +3,13 @@ import { Client } from './client'
 import { EVENT, CONNECTION_STATE } from './constants'
 import * as C from '../binary-protocol/src/message-constants'
 
-const client = (url: string, options?: any): Client => {
+export class ClientFactory extends Client {
+  public static CONNECTION_STATE: typeof CONNECTION_STATE = CONNECTION_STATE
+  public static C: typeof C = C
+  public static EVENT: typeof EVENT = EVENT
+  public static deepstream: typeof ClientFactory = ClientFactory
+
+  public static createInstance (url: string, options?: any): Client {
     return new Client(url, options)
+  }
 }
-export = Object.assign(client, {
-    CONNECTION_STATE,
-    C,
-    EVENT,
-    deepstream: client
-})
