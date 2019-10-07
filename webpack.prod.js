ccc05b67dd50f156877a7f9154fa302953ec71ca
Yasser Fadl <yasser.fadl@gmail.com>
2019-10-07 12:39:26 +0200

    build: use terser instead of uglify (same efficiency)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,15 @@
-const devConfig = require('./webpack.dev');
+const devConfig = require('./webpack.dev')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     ...devConfig,
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+            },
+        })],
+    },
     output: {
         ...devConfig.output,
         filename: './dist/bundle/ds.min.js'
