198a9b6ba60e7378fd32bfea6c831f9e79ee04d0
jaime-ez <jechegarayz@gmail.com>
2020-06-28 22:42:14 -0400

    [fix] explicit Buffer import

--- src/connection/socket-factory.ts
+++ src/connection/socket-factory.ts
@@ -1,3 +1,4 @@
+import { Buffer } from 'buffer'
 import { parse } from '@deepstream/protobuf/dist/src/message-parser'
 import { getMessage } from '@deepstream/protobuf/dist/src/message-builder'
 import {Socket} from '../deepstream-client'
